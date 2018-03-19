40f2664dd756b3dbd73c629bcfc1add796b9a2b0
Susmitha27 <33645291+Susmitha27@users.noreply.github.com>
2018-03-19 11:15:52 +0530

    Delete open_file.py

--- open_file.py
+++
@@ -1,8 +0,0 @@
-import Windows_search
-import os
-from Windows_search import winsearch
-def open(string):
-    path = winsearch(string)
-    if not path =="":
-        #os.system("start string")
-        os.startfile(path)
